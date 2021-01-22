d6c4e246e509ba5261d25d6dc055f8ebb2a956b3
MouZaiSi <69446758+MouZaiSi-Demo@users.noreply.github.com>
2021-01-22 20:04:54 +0800

    Add files via upload

---
+++ L1-003 个位数统计 (15分).cpp
@@ -0,0 +1,17 @@
+#include <iostream> 
+#include <string> 
+using namespace std;
+
+int main(void)
+{
+    string s; cin >> s;
+    int len = s.length(); int a[10] = { 0 };
+    for (int i = 0; i < len; i++) a[s[i] - '0']++;
+
+    for (int i = 0; i < 10; i++)
+    {
+        if (a[i] != 0) cout << i << ":" << a[i] << endl;
+    }
+
+    return 0;
+}
